ef8c4697486789c3fee215ecee16715fff43dac5
birewon <birewons@gmail.com>
2026-05-18 08:46:40 +0300

    Fixed monitors settings

--- scripts/auto_resolution.sh
+++ scripts/auto_resolution.sh
@@ -3,7 +3,7 @@
 LUST_STATUS=false
 while true; do
     STATUS=$(cat /sys/class/power_supply/ACAD/online)
-    if [[ $STATUS != $LUST_STATUS ]]; then
+    if [[ $STATUS != $LUST_STATUS ]] && [[ ! -f /tmp/hdmi_active ]]; then
         if [[ $STATUS == 1 ]]; then
             hyprctl keyword monitor "eDP-1, 2560x1600@60, 0x0, 2"
         else
--- scripts/monitors.sh
+++ scripts/monitors.sh
@@ -3,15 +3,24 @@
 STATE="main" 
 
 while true; do
-    sleep 2
-    if hyprctl monitors | grep -q "HDMI-A-1" && [[ $STATE == "main" ]]; then
+    if hyprctl monitors all | grep -q "HDMI-A-1" && [[ $STATE == "main" ]]; then
+        touch /tmp/hdmi_active
+        hyprctl keyword monitor "HDMI-A-1, enable" > /dev/null
+        hyprctl keyword monitor "HDMI-A-1, 1920x1080@100, auto, 1" > /dev/null
         hyprctl keyword monitor "eDP-1, disable" > /dev/null
-        hyprctl keyword monitor "HDMI-A-1, enable"
         STATE="external"
     elif ! hyprctl monitors | grep -q "HDMI-A-1" && [[ $STATE == "external" ]]; then
-        hyprctl keyword monitor "eDP-1, highrr, auto, 2" > /dev/null
-        hyprctl keyword monitor "HDMI-A-1, disable"
+        rm -f /tmp/hdmi_active
+        STATUS=$(cat /sys/class/power_supply/ACAD/online)
+        if [[ $STATUS == 1 ]]; then
+            hyprctl keyword monitor "eDP-1, 2560x1600@60, 0x0, 2" > /dev/null
+        else
+            hyprctl keyword monitor "eDP-1, 1920x1200@60, 0x0, 1.5 > /dev/null"
+        fi
+
+        hyprctl keyword monitor "HDMI-A-1, disable" > /dev/null
         STATE="main"
     fi
+    sleep 2
 done 
     
